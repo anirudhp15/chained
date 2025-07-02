9979fe4919e052de95eac5c80f4b1b9dec6285a7
Ani Pottammal <ap7564@nyu.edu>
2025-07-02 04:21:20 -0400

    Fix Convex auth domain to match Clerk Frontend API URL

--- convex/auth.config.ts
+++ convex/auth.config.ts
@@ -1,7 +1,7 @@
 export default {
   providers: [
     {
-      domain: "https://absolute-shepherd-88.clerk.accounts.dev",
+      domain: "https://clerk.chained.chat",
       applicationID: "convex",
     },
   ],
